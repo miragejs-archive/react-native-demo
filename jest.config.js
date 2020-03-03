bc7c0e1802200ccdad506ea711048d6be49a62c7
Ryan Toronto <ryanto@gmail.com>
2020-03-02 19:39:25 -0500

    use setupFiles

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,6 @@ const jestPreset = require("@testing-library/react-native/jest-preset");
 
 module.exports = {
   preset: "jest-expo",
-  // setupFiles: [...jestPreset.setupFiles],
+  setupFiles: [...jestPreset.setupFiles],
   setupFilesAfterEnv: ["./jest.setup.js"]
 };
